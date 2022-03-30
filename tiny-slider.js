b72003b45f02fea5ccc9ee9359fced24654f6ddd
Kim Jiyeon <47101719+ji9318@users.noreply.github.com>
2022-03-31 00:21:33 +0900

    💄 CSS: Event

---
+++ tiny-slider.js
@@ -0,0 +1,12 @@
+var slider1 = tns({
+  container: ".event-slider",
+  items: 1,
+  speed: 400,
+  slideBy: "page",
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  mode: "gallery",
+  animateIn: "fadeInDown",
+  // animateOut: "fadeOutDown",
+});
