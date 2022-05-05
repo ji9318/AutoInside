dc2a260bd31025b6fc44a1184ed3aa2951bfcccb
Kim Jiyeon <47101719+ji9318@users.noreply.github.com>
2022-05-06 00:55:31 +0900

    💄 CSS: Footer

--- tiny-slider.js
+++ tiny-slider.js
@@ -3,10 +3,48 @@ var slider1 = tns({
   items: 1,
   speed: 400,
   slideBy: "page",
+  mouseDrag: true,
   autoplay: true,
   controls: false,
   autoplayButtonOutput: false,
   mode: "gallery",
   animateIn: "fadeInDown",
-  // animateOut: "fadeOutDown",
+});
+
+var slider2 = tns({
+  container: ".play-slider",
+  items: 1,
+  mouseDrag: true,
+  loop: false,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  autoplayButtonOutput: false,
+  edgePadding: 20,
+  startIndex: 1,
+  center: true,
+  responsive: {
+    750: {
+      edgePadding: 80,
+    },
+    1200: {
+      edgePadding: 200,
+    },
+  },
+});
+
+var slider3 = tns({
+  container: ".issue-slider",
+  items: 2.3,
+  mouseDrag: true,
+  loop: false,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  autoplayButtonOutput: false,
+  responsive: {
+    750: {
+      fixedWidth: 313,
+    },
+  },
 });
